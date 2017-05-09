3db30cc00092e48a98c68919c9dcada914410368
MLangteau <langteauM@gmail.com>
2017-05-08 22:32:34 -0400

    changing more files - first stages

--- config/connection.js
+++ config/connection.js
@@ -1,30 +1,44 @@
-// Set up MySQL connection for Node
-var mysql = require("mysql");
+// ***********************************************************************************
+//  connection.js sets up MySQL connection for Nodeinitiates the connection to MYSQL
+// ***********************************************************************************
+ 
+var Sequelize = require("sequelize");
 
-// Set up database and necessities for connection online and locally
-var connection;
-
-if(process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-    }
-else {
-    connection = mysql.createConnection({
-        port: 3306,
-        host: "localhost",
-        user: "root",
-        password: "root",
-        database: "burgers_db"
-    });
-};
-
-// Make connection and display threadId for verification of connection
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
+// Creates MySQL connection using Sequelize
+//
+var sequelize = new Sequelize("seq_burger", "root", "root", {
+  host: "localhost",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
   }
-  console.log("connected as id " + connection.threadId);
 });
 
+//var connection;
+
+// if(process.env.JAWSDB_URL) {
+//     connection = mysql.createConnection(process.env.JAWSDB_URL);
+//     }
+// else {
+//     connection = mysql.createConnection({
+//         port: 3306,
+//         host: "localhost",
+//         user: "root",
+//         password: "root",
+//         database: "burgers_db"
+//     });
+// };
+
+// Make connection and display threadId for verification of connection
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
 // Export connection for our ORM to use.
-module.exports = connection;
+module.exports = sequelize;
